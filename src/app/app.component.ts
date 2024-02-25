12c6d02864124e77aeb88aaf8e8ee4f53e4db072
capriciousbased <mdanisulhoquekhan@gmail.com>
2024-02-25 16:11:40 +0100

    added error message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,19 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   newMemberName = '';
   members: string[] = [];
+  errorMessage = '';
+
   onInput(member: string) {
     this.newMemberName = member;
-    console.log(this.newMemberName);
   }
+
   addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = 'Name cannot be empty!';
+      return;
+    }
     this.members.push(this.newMemberName);
-    console.log(this.members);
+    this.newMemberName = '';
+    this.errorMessage = '';
   }
 }
